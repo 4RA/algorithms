66acfdceb95239b329f12fa10b8489f5bf7d9837
ra4 <rrrr4ra@sookmyung.ac.kr>
2022-03-29 23:37:12 +0900

    #220329 codeup 6026 to 6050

---
+++ codeup/6026to6050.py
@@ -0,0 +1,14 @@
+#2022.03.29
+
+
+#6026
+a=float(input())
+b=float(input())
+
+print(a+b)
+
+#6027
+num=int(input())
+print("%x"%num);
+
+#6028
--- codeup_6026to6050.py
+++
@@ -1,8 +0,0 @@
-#2022.03.29
-
-
-#6026
-a=float(input())
-b=float(input())
-
-print(a+b)
