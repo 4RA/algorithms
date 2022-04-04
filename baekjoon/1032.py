b58d67e00fd62b4d866d5c0051f08a32e9d2e1cf
4ra <rrrr4ra@sookmyung.ac.kr>
2022-04-04 21:07:24 +0900

    #명령 프롬프트

--- baekjoon/1032.py
+++ baekjoon/1032.py
@@ -1,4 +1,13 @@
-num = int(input())
-list = []
-for i in range num:
-    list[i] = input()
+n=int(input())
+a = list(input())
+a_len = len(a)
+
+
+
+for i in range(n-1):
+    b=list(input())
+    for j in range(a_len):
+        if(a[j] != b[j]):
+            a[j]='?'
+
+print(''.join(a))
